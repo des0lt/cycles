6ded1e384bbdd801d969a0ed2dc9ba799aa9ab6d
Roman <romantkachenko533@gmail.com>
2025-01-06 23:12:13 +0200

    last

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 "use strict"
 
-let result = "";
-const length = 6;
-
-for (let i = 1; i <= length; i++) {
-    for (let j = 1; j <= i; j++) {
-        result += "*"
-    }
-    result += "\n";
+for (let i = 5; i <= 10; i++) {
+    console.log(i)
 }
 
-console.log(result);
+for (let i = 20; i >= 10; i--) {
+    if (i === 13) {
+        break;
+    }
+    console.log(i);
+}
+
+for (let i = 2; i <= 10; i++){
+    if(i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+let i= 2;
+
+while (i <= 16) {
+    if (i % 2 === 0) {
+        i++;
+        continue;
+    } else {
+        console.log(i);
+    }
+    i++;  
+}
+
+const arrMain = [];
+
+for (let i = 5; i <= 10; i++) {
+    arrMain[i - 5] = i;
+}
+
+console.log(arrMain);
